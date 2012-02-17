4b9515151cf7d294299d43ce5d6dc8fb71a8749f
David Gibb <degibb@gmail.com>
2012-02-16 22:34:56 -0800

    Changed file to use double instead of single quotes. oops

--- extras/extra.zsh
+++ extras/extra.zsh
@@ -1,1 +1,19 @@
+# otherwise oh my zsh is unbearably slow
 setopt NO_cdable_vars
+
+# oh my zsh stuff
+ZSH_THEME="arrow"
+plugins=(git)
+
+alias drupaltag="ctags --langmap=php:.engine.inc.module.theme.install.php.test --php-kinds=cdfi --languages=php --recurse"
+alias ack="ack-grep"
+alias mmv="noglob zmv -W"
+
+# ack options
+export ACK_OPTIONS="--type-set php=.php,.module,.inc,.install"
+export ACK_PAGER="less -R"
+
+# add ruby gems
+export PATH="/var/lib/gems/1.8/bin:$HOME/bin:$PATH"
+
+export EDITOR="vim"
